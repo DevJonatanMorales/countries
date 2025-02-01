a33321e1313e6b12bcfa9a1d1c70405c4ed43b22
devjonatanmo <developerjonatanmorales@gmail.com>
2025-02-01 11:13:26 -0600

    chore(core): se actualizo el componente que se mostrar en la ruta "/pais"

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router";
 import "./assets/styles/styles.scss";
 import { HomePage } from "./page/homePage";
 import { NotFound } from "./page/NotFound";
+import { Country } from "./page/country";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/pais" element={<HomePage />} />
+      <Route path="/pais" element={<Country />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
