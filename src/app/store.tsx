8c3cd6fe1a601bed2790d6333823bdc733bc4ee2
devjonatanmo <developerjonatanmorales@gmail.com>
2025-02-01 11:18:09 -0600

    chore(config): se agrego countrySlice slice al store

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "../features/countries";
 import { countriesApi } from "../services/countries";
+import { countrySlice } from "../features/country";
 
 export const store = configureStore({
   reducer: {
     [countriesApi.reducerPath]: countriesApi.reducer,
     counter: counterSlice.reducer,
+    country: countrySlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(countriesApi.middleware),
