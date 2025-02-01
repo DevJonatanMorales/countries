fe5541146a14e1f3656250ec50f6d2da6c74ead4
devjonatanmo <developerjonatanmorales@gmail.com>
2025-02-01 11:16:02 -0600

    chore(core): se agrego la funcinalidad de redirigir a la ruta encargada de mostrar el pais

--- src/components/cardPais/cardPais.tsx
+++ src/components/cardPais/cardPais.tsx
@@ -1,15 +1,28 @@
+import { useDispatch } from "react-redux";
+import { setCountry } from "../../features/country";
 import { Country } from "../../interfaces/Country";
+import { useNavigate } from "react-router";
 
 interface PropCard {
   pais: Country;
 }
 
 export const CardPise = ({ pais }: PropCard) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    dispatch(setCountry(pais));
+    navigate("/pais");
+  };
+
   return (
     <section className="card">
       <img className="card__img" src={pais.flags.svg} alt={pais.name.common} />
       <main className="card__content">
-        <h1 className="card__title">{pais.name.common}</h1>
+        <h1 className="card__title" onClick={handleClick}>
+          {pais.name.common}
+        </h1>
         <p className="card__paragraph">Population: {pais.population}</p>
         <p className="card__paragraph">Region: {pais?.region}</p>
         <p className="card__paragraph">Capital: {pais.capital}</p>
