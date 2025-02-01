968b49c652d1b7a27ddf34648c3af94e38d9ce98
devjonatanmo <developerjonatanmorales@gmail.com>
2025-02-01 11:18:43 -0600

    feat(config): se creo el countrySlice

---
+++ src/features/country.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Country } from "../interfaces/Country";
+
+const initialState: Partial<Country> = {};
+
+export const countrySlice = createSlice({
+  name: "country",
+  initialState,
+  reducers: {
+    setCountry: (state, action: PayloadAction<Country>) => {
+      console.log(action.payload);
+      return action.payload;
+    },
+    clearCountry: (state) => {
+      Object.assign(state, initialState);
+    },
+  },
+});
+
+export const { setCountry, clearCountry } = countrySlice.actions;
+export default countrySlice.reducer;
