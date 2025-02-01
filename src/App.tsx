14fe4071ab5d93c7f759caf497ed7f14df129c6c
devjonatanmo <developerjonatanmorales@gmail.com>
2025-02-01 10:17:08 -0600

    feat(core): se implemento el enrutamiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router";
 import "./assets/styles/styles.scss";
 import { HomePage } from "./page/homePage";
+import { NotFound } from "./page/NotFound";
+
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/pais" element={<HomePage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
