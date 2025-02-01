3dd35f39830dc9cdc7bf2fbc7692f02cb31bc856
devjonatanmo <developerjonatanmorales@gmail.com>
2025-02-01 04:45:42 -0600

    chore(core): se creo el componente cardPais

--- src/components/cardPais/cardPais.tsx
+++ src/components/cardPais/cardPais.tsx
@@ -1,10 +1,19 @@
-export const CardPise = ({ pais }) => {
+import { Country } from "../../interfaces/Country";
+
+interface PropCard {
+  pais: Country;
+}
+
+export const CardPise = ({ pais }: PropCard) => {
   return (
-    <article>
-      <h1>PASI: {pais.name.common}</h1>
-      <p>Population: {pais.population}</p>
-      <p>Region: {pais?.region ?? "no hay region"} </p>
-      <p>Capital: {pais.capital}</p>
-    </article>
+    <section className="card">
+      <img className="card__img" src={pais.flags.svg} alt={pais.name.common} />
+      <main className="card__content">
+        <h1 className="card__title">PASI: {pais.name.common}</h1>
+        <p className="card__paragraph">Population: {pais.population}</p>
+        <p className="card__paragraph">Region: {pais?.region}</p>
+        <p className="card__paragraph">Capital: {pais.capital}</p>
+      </main>
+    </section>
   );
 };
