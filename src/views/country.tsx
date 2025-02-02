9e5c6138787822d61315c5aa4098fa56696dfa27
devjonatanmo <developerjonatanmorales@gmail.com>
2025-02-01 19:01:17 -0600

    feat(core): se crear el componente CountryView para mostrar el país y el encabezado

---
+++ src/views/country.tsx
@@ -0,0 +1,11 @@
+import { Country } from "../components/country/Country";
+import { Header } from "../components/header/Header";
+
+export const CountryView = () => {
+  return (
+    <>
+      <Header />
+      <Country />
+    </>
+  );
+};
